9ffcd7381aee4800492af18cd60baf9373486dac
João Victor Santana <113112134+JoaoVVSantana@users.noreply.github.com>
2025-01-02 14:25:28 -0300

    Update Program.cs

--- SistemaLembretes/SistemaLembretes.Server/Program.cs
+++ SistemaLembretes/SistemaLembretes.Server/Program.cs
@@ -5,15 +5,12 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Configurar os serviços
 builder.Services.AddDbContext<DatabaseContext>(options =>
     options.UseSqlite("Data Source=lembretes.db"));
-
 builder.Services.AddScoped<IRepositorioLembretes, RepositorioLembretes>();
 builder.Services.AddScoped<IGerenciadorLembretes, GerenciadorLembretes>();
 builder.Services.AddControllers();
 
-// Configurar o pipeline de requisição
 var app = builder.Build();
 
 app.MapControllers();
